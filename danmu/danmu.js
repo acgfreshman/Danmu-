2c9d58bfe47c19dcd36a2d99446334b2465f9c89
acgfreshman <liumuchen1996@gmail.com>
2017-03-10 11:55:35 -0800

    Build danmu generation function without animiation

--- danmu/danmu.js
+++ danmu/danmu.js
@@ -11,5 +11,33 @@ function randomColor(){
     return color;
 }
 
+//submit button create a new danmu and append it to the screen
+function generateDanmu(){
+    //get the input content
+    var input = document.getElementById("input").value;
+    //create a new textnode
+    var text = document.createTextNode(input);
+    if(input === "")
+	alert("You must say something");
+    else{
+    //create the danmu object	
+    //create a span to wrap the textnode then apply style to it
+    var span = document.createElement("span");
+    span.style.fontSize = "15px";
+    span.style.color= randomColor();
+    span.appendChild(text);
+    //get the danmu element
+    var danmu = document.getElementById("dm_screen");
+    danmu.appendChild(span);	
+    }
+    //clear the input area
+    document.getElementById("input").value = "";
+}
 
-
+//clear button clear the screen 
+function clearDanmu(){
+    var danmu = document.getElementById("dm_screen");
+    while(danmu.firstChild){
+	danmu.removeChild(danmu.firstChild);
+    }
+}    
