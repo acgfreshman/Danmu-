49807c700247fcb41e7dd4f162ce0a408d53b0a5
acgfreshman <liumuchen1996@gmail.com>
2017-03-10 16:41:16 -0800

    Build basic danmu animation, bugs with long strings and strings with unicode

--- danmu/danmu.js
+++ danmu/danmu.js
@@ -1,15 +1,24 @@
 //random color generator for danmu color
-function randomEightBit(){
-    return Math.floor(Math.random() * 256 + 1);
+function randomNum(val){
+    return Math.floor(Math.random() * val + 1);
 }
 
 function randomColor(){
-    var r = randomEightBit();
-    var g = randomEightBit();
-    var b = randomEightBit();
+    var r = randomNum(256);
+    var g = randomNum(256);
+    var b = randomNum(256);
     var color = "rgb(" + r + "," + g + "," + b +")";
     return color;
 }
+
+//clear button clear the screen 
+function clearDanmu(){
+    var danmu = document.getElementById("dm_screen");
+    while(danmu.firstChild){
+	danmu.removeChild(danmu.firstChild);
+    }
+}
+
 
 //submit button create a new danmu and append it to the screen
 function generateDanmu(){
@@ -20,24 +29,52 @@ function generateDanmu(){
     if(input === "")
 	alert("You must say something");
     else{
-    //create the danmu object	
-    //create a span to wrap the textnode then apply style to it
-    var span = document.createElement("span");
-    span.style.fontSize = "15px";
-    span.style.color= randomColor();
-    span.appendChild(text);
-    //get the danmu element
-    var danmu = document.getElementById("dm_screen");
-    danmu.appendChild(span);	
+	//get the danmu element
+	var danmu = document.getElementById("dm_screen");
+	var page = document.getElementById("page");
+	//create the danmu object	
+	//create a span to wrap the textnode then apply style to it
+	var container = document.createElement("div");
+	container.appendChild(text);
+	container.style.fontSize = "30px";
+	container.style.color= randomColor();
+	//put danmu at the rightmost place with random height
+	var properHeight = danmu.offsetHeight - 40;
+	var textTop = randomNum(properHeight)  + "px";
+	var textLeft = danmu.offsetWidth + "px";
+	container.style.position = "absolute";
+	container.style.left = textLeft;
+	container.style.top = textTop;
+	danmu.appendChild(container);
+	//get the style.left of the danmu 
+	var pos = parseInt(textLeft);
+	//set animation
+	var interval = setInterval(frame,5);
+	function frame(){
+	    if (pos < 0) {
+		//if go out of boundary danmu will disappear
+		container.innerHTML = "";
+		clearInterval(interval);
+	    } else {
+		pos--; 
+		container.style.left = pos + 'px'; 
+	    }
+	}
     }
+    
     //clear the input area
     document.getElementById("input").value = "";
 }
 
-//clear button clear the screen 
-function clearDanmu(){
-    var danmu = document.getElementById("dm_screen");
-    while(danmu.firstChild){
-	danmu.removeChild(danmu.firstChild);
+
+//press enter can submit danmu rather than click the button
+document.onkeydown = function() {
+    if(window.event.keyCode == "13") {
+	generateDanmu();
     }
-}    
+}
+
+
+
+
+
