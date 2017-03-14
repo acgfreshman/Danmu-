62ad47f34192c73d154eb4cf5ba489f6de988661
acgfreshman <liumuchen1996@gmail.com>
2017-03-13 17:49:43 -0700

    Finish the option panel

--- danmu/danmu.js
+++ danmu/danmu.js
@@ -43,6 +43,7 @@ function getSize(){
 }
 
 function getColor(){
+    changeColor = true;
     var color = document.getElementById("fontColor").value;
     return color;
 }
@@ -93,20 +94,23 @@ function updateOpacity(){
 
 function updateSpeed(){
     document.getElementById("speedPanel").innerHTML = document.getElementById("fontSpeed").value;
+    myBullet.speed = getSpeed();
     updatePreview(myBullet);
 }
 
 function updateStyle(){
+    myBullet.style = getStyle();
     updatePreview(myBullet);
 }
 
 function updateFontFamily(){
+    myBullet.font = getFont();
     updatePreview(myBullet);
 }
 
 //update the preview panel if the option is updated
 function updatePreview(bullet){
-    var content = "123!@#";
+    var content = "123ABC";
     generateCustom(content,bullet);
 }
 
@@ -117,8 +121,10 @@ var color = getColor();
 var opacity = getOpacity();
 var speed = getSpeed();
 var style = getStyle();
-var font = getFont();
+var font = getFont() //record the browser default font depending on which browser is used
 var myBullet = new bullet(size,color,opacity,speed,style,font);
+var changeColor = false; //check if the color is changed otherwise use random color
+//get a copy of default Bullet
 
 
 
@@ -129,7 +135,50 @@ function generateCustom(content,bullet){
     container.style.fontSize = bullet.size + "px";
     container.style.color = bullet.color;
     container.style.opacity = bullet.opacity;
+    if(bullet.style === "bold"){
+	container.style.fontStyle = "normal";
+	container.style.fontWeight = "bold";
+    }
+    else if (bullet.style === "italic"){
+	container.style.fontStyle = "italic"
+	container.style.fontWeight = "normal";
+    }
+    else {
+	container.style.fontStyle = "normal";
+	container.style.fontWeight = "normal";
+    }
+    if(bullet.font === "Roboto"){
+	container.style.fontFamily = "Roboto,sans-serif";
+    }
+    else if(bullet.font === "Baloo Bhaina")
+	container.style.fontFamily = "Baloo Bhaina, cursive";
+    else
+	container.style.fontFamily = "Times New Roman,Times,serif";
 }
+//define close button in the modal
+function closeModal(){
+//reset the input to default
+    document.getElementById("fontSize").value = "30";
+    document.getElementById("fontColor").value = "#000000";
+    document.getElementById("fontOpacity").value = "100";
+    document.getElementById("fontSpeed").value = "1";
+    document.getElementById("default").checked = true;
+    document.getElementById("bold").checked = false;
+    document.getElementById("italic").checked = false;
+    document.getElementById("fontBold").className = "btn btn-default fontStyleBold";
+    document.getElementById("fontItalic").className = "btn btn-default fontStyleItalic";
+    document.getElementById("fontDefault").className = "btn btn-default active";
+    document.getElementById("fontFamily").value = "Default";
+    
+//update all the panel and bullet object
+    updateSize();
+    updateColor();
+    updateOpacity();
+    updateSpeed();
+    updateStyle();
+    updateFontFamily();
+}
+
 
 //submit button create a new danmu and append it to the screen
 function generateDanmu(){
